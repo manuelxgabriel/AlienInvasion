7bb8f07c7430c7aba3587314ae097c63d4ed20bf
Manuel <manuelxgabriel@gmail.com>
2025-07-19 08:14:23 -0700

    fix the merge conflict

--- settings.py
+++ settings.py
@@ -1,2 +1,11 @@
     def __init__(self):
         """Initialize the game settings"""
+class Settings:
+    """Class that stores all the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        # Screen setting
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = "#b8b3e9"
